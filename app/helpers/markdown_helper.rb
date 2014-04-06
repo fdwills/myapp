a630769bf4a7847a1274d0e87907b05547e33d00
wei.renzhong <renzhong.wei@dena.com>
2014-04-06 15:12:49 +0900

    fix

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,8 +1,21 @@
 module MarkdownHelper
   def markdown(text)
     unless @markdown
+      options = {
+        hard_wrap: true,
+        filter_html: true,
+        autolink: true,
+        no_intra_emphasis: true,
+        strikethrough: true,
+        fenced_code_blocks: true,
+        gh_blockcode: true,
+        tables: true,
+        footnotes: true,
+        lax_spacing: true
+      }
+
       renderer = Redcarpet::Render::HTML.new
-      @markdown = Redcarpet::Markdown.new(renderer, tables: true)
+      @markdown = Redcarpet::Markdown.new(renderer, options)
     end
 
     @markdown.render(text).force_encoding("UTF-8").html_safe
